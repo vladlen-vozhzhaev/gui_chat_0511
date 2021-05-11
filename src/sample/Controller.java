6eed93c34f53a65abf474c0dc4fab3ad4875d25b
Vladlen <vladlen@vozhzhaev.ru>
2021-05-11 15:27:37 +0300

    Сериализация

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,6 +12,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
@@ -40,26 +41,35 @@ public class Controller {
     @FXML
     private void connect(){
         try {
-            socket = new Socket("192.168.1.5",8188);
+            socket = new Socket("localhost",8188);
             DataInputStream in =new DataInputStream(socket.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             out=new DataOutputStream(socket.getOutputStream()); // Инициализация out
-            String response = in.readUTF(); // Ждём сообщение от сервера
-            textArea.appendText(response+"\n"); // Введите имя
+            try {
+                String response = ois.readObject().toString(); // Ждём сообщение от сервера
+                textArea.appendText(response+"\n"); // Введите имя
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     while(true){
                         try {
-                            String response = in.readUTF(); // ждём сообщение от сервера
-                            if(response.indexOf("**userlist**")==0){
-                                textAreaUserList.clear();
-                                String[] userList = response.split("//");
-                                for (String userName:userList) {
-                                    textAreaUserList.appendText(userName+"\n");
+                            Object object = ois.readObject();
+                            if(String.class == object.getClass()){ // Если нам прислали текстовое сообщение
+                                textArea.appendText(object.toString()+"\n"); // То печатаем его на TextArea
+                            }else if(ArrayList.class ==  object.getClass()){ //  Если нам прислали объект класса ArrayList
+                                ArrayList<String> usersName = new ArrayList<String>(); // то это список пользователей
+                                usersName = (ArrayList<String>) object; // Преобразуем объект в ArrayList
+                                textAreaUserList.clear(); // Очищаем поле для списка имён
+                                for (String userName: usersName) { // Перебираем список
+                                    textAreaUserList.appendText(userName+"\n"); // И ишем их на экран
                                 }
                             }else{
-                                textArea.appendText(response+"\n");
+                                System.out.println("Класс не определен");
                             }
+
                         } catch (Exception exception) {
                             exception.printStackTrace();
                         }
